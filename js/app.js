a46e91579753acec57600581a72238e6ac38fe8d
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-08-22 13:39:24 +0700

    kskkssk

--- js/app.js
+++ js/app.js
@@ -18,53 +18,6 @@ $(document).ready(function() {
 //     }
 //   }
 // })
-
-gsap.timeline({
-    scrollTrigger: {
-      trigger: ".box",
-      start: "center center",
-      end: "bottom top",
-      scrub: 1,
-      pin:true
-    }
-  })
-  .from(".text1", {
-    x: innerWidth * 1  
-  })
-  .from(".text2", {
-    x: innerWidth * -1  
-  })
-  .from(".text3", {
-    x: innerWidth * 1  
-  })
-  .from(".logo", {
-    y: innerHeight * 1,
-    rotate:360
-  })
-  
-  
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: ".box2",
-      start: "center center",
-      end: "bottom top",
-      scrub: 1,
-      pin:true
-    }
-  })
-  .from(".box2", {
-    opacity: 0,
-  })
-  .from(".text4", {
-    y: innerHeight * 1,
-    stagger : {
-      amount : 0.5,
-    }
-  })  
-  
-  
-  
-
 // $(".data-detail").hide()
 $(".gambar").click(function(){
     var id_ = $(this).attr('id')
